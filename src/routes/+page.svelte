1d0b80ce744e7ab23ca33f332c67f12c92b66659
qyct <rukmangadan.d@gmail.com>
2025-02-04 14:25:18 +0530

    p

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<div class="m-4 rounded bg-gray-300 p-4">
+  <div class="font-bold">
+    <span>QA / QC Tool</span>
+    <a class="bg-blue-600 hover:bg-amber-600 ml-4 p-1 rounded text-white text-xs" href="https://www.7-zip.org/a/7z2409-x64.exe" target="_blank">Download</a>
+  </div>
+  <div class="text-sm mt-2">Assess quality of ORI/DEM through systematic checks, ensuring consistency and compliance with standards for decision-making.</div>
+  
+</div>
+
+<div class="fixed bottom-0 bg-amber-700 h-8 w-full p-2 text-white flex items-center justify-between">
+  <span>GeoCrate</span>
+  <span>&copy; {new Date().getFullYear()}</span>
+</div>
