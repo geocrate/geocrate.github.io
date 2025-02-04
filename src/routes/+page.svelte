3176b62c641a463fefa5e44e6621bbf565144ec4
qyct <rukmangadan.d@gmail.com>
2025-02-04 14:28:58 +0530

    p

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="m-4 rounded bg-gray-300 p-4">
   <div class="font-bold">
     <span>QA / QC Tool</span>
-    <a class="bg-blue-600 hover:bg-amber-600 ml-4 p-1 rounded text-white text-xs" href="https://www.7-zip.org/a/7z2409-x64.exe" target="_blank">Download</a>
+    <a class="bg-amber-700 hover:bg-amber-600 ml-4 p-1 rounded text-white text-xs" href="https://www.7-zip.org/a/7z2409-x64.exe" target="_blank">Download</a>
   </div>
   <div class="text-sm mt-2">Assess quality of ORI/DEM through systematic checks, ensuring consistency and compliance with standards for decision-making.</div>
   
